d39f91e4ef84892f54bcb7d6538903d9269a144e
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-02-22 12:07:53 -0800

    This is placing the data into state, stuff is still in the app.js until I figure out how to make it work in the module files.

--- client/components/TempToBrew.js
+++ client/components/TempToBrew.js
@@ -4,20 +4,20 @@ module.exports = function () {
   var TempToBrew = React.createClass({
     getInitialState: function () {
       return {
-        stepper: 120
+        brew: 120
       }
     },
     stepDown: function () {
-      this.setState({stepper : this.state.stepper - 10});
+      this.setState({brew : this.state.brew - 10});
     },
     stepUp: function () {
-      this.setState({stepper : this.state.stepper + 10});
+      this.setState({brew : this.state.brew + 10});
     },
     render: function () {
       return (
         <div>
-          <h2>Temperature to Brew:</h2>
-          <h3>{this.state.stepper}</h3>
+          <h2>Temperature to Brew</h2>
+          <h3>{this.state.brew}</h3>
           <div>
             <button type="button" name="tempDown" onClick={this.stepDown}>-</button>
             <button type="button" name="tempUp" onClick={this.stepUp}>+</button>
--- client/components/TimeToBrew.js
+++ client/components/TimeToBrew.js
@@ -4,20 +4,20 @@ module.exports = function (){
   var TimeToBrew = React.createClass({
     getInitialState: function () {
       return {
-        stepper: 3
+        steep: 3
       }
     },
     stepDown: function () {
-      this.setState({stepper : this.state.stepper - 1});
+      this.setState({steep : this.state.steep - 1});
     },
     stepUp: function () {
-      this.setState({stepper : this.state.stepper + 1});
+      this.setState({steep : this.state.steep + 1});
     },
     render: function () {
       return (
         <div>
           <h2>Time To Brew</h2>
-          <h3>{this.state.stepper}</h3>
+          <h3>{this.state.steep}</h3>
           <div>
             <button type="button" name="steepDown" onClick={this.stepDown}>-</button>
             <button type="button" name="steepUp" onClick={this.stepUp}>+</button>
